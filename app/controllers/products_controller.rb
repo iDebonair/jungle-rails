eb9b520158a4a26d3fc79600ee1aeeeca7365686
iDebonair <ofemiowolabi@gmail.com>
2023-09-26 19:35:35 -0600

    modified products card to show products that are sold out

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,16 @@ class ProductsController < ApplicationController
     @product = Product.find params[:id]
   end
 
+  def add_item_cart_path
+    @product = Product.find params[:id]
+    
+    if @product.quantity > 0
+      @product.quantity -= 1
+      @product.save
+      # Add the item to the cart here if needed.
+    end
+
+    redirect_back fallback_location: root_path
+  end
+
 end
