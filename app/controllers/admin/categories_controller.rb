65d990261e31692bb4ccf63273b06e4852bec6f3
iDebonair <ofemiowolabi@gmail.com>
2023-09-27 19:56:02 -0600

    Included categories in the admin profile

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,34 @@
+class Admin::CategoriesController < ApplicationController
+    before_action :authenticate
+
+    def index
+        @categories = Category.all
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            redirect_to admin_categories_path, notice: 'Category has been created successfully'
+
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+        def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['username'] && password == ENV['password']
+      end
+    end
+end
